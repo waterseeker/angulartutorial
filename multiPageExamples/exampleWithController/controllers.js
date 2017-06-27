7c787e4737df12471ec4086c2cfb3e45ee177820
Wayne Burris <waterseeker@hotmail.com>
2017-06-27 11:40:33 -0500

    added some more examples using expressions

--- multiPageExamples/exampleWithController/controllers.js
+++ multiPageExamples/exampleWithController/controllers.js
@@ -1,6 +1,6 @@
 
         var app = angular.module('myApp', []);
         app.controller('myCtrl', function ($scope) {
-            $scope.firstName = "John";
+            $scope.firstName = "John"; //sets the initial value of firstName within this $scope
             $scope.lastName = "Doe";
         });
